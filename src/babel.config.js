e9f750f12cd6b99d796d8e7bcb4db10fef8de5fe
Kaio <kaio_smoreira@hotmail.com>
2021-09-30 16:17:59 -0300

    alter file babel

--- src/babel.config.js
+++ src/babel.config.js
@@ -5,10 +5,10 @@ module.exports = {
       "module-resolver",
       {
         alias: {
-          "@modules": ["./src/modules"],
-          "@config": ["./src/config"],
-          "@shared": ["./src/shared"],
-          "@utils": ["./src/utils"],
+          "@modules": "./src/modules",
+          "@config": "./src/config",
+          "@shared": "./src/shared",
+          "@utils": "./src/utils",
         },
       },
       "babel-plugin-transform-typescript-metadata",
