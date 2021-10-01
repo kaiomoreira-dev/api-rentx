8b9417871b2b378fbd3d90816bf9930030109005
Kaio <kaio_smoreira@hotmail.com>
2021-10-01 16:30:12 -0300

    alter outhers files

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,8 +1,7 @@
 import { Router } from "express";
 
+import { AuthenticateUserController } from "@modules/accounts/useCases/authenticateUser/AuthenticateUserController";
 import { RefreshTokenController } from "@modules/accounts/useCases/refreshToken/RefreshTokenController";
-
-import { AuthenticateUserController } from "../../../../src/modules/accounts/useCases/authenticateUser/AuthenticateUserController";
 
 const authenticateRoutes = Router();
 
