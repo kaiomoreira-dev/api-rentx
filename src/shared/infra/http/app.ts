6554f441425447cdab9c9950c1d9c0edc9af0f30
Kaio <kaio_smoreira@hotmail.com>
2021-10-15 17:30:23 -0300

    install Sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -5,6 +5,9 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUI from "swagger-ui-express";
 
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
+
 import "../../container";
 
 import upload from "@config/upload";
@@ -17,6 +20,21 @@ import { router } from "./routes";
 
 createConnection();
 const app = express();
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+        new Sentry.Integrations.Http({ tracing: true }),
+
+        new Tracing.Integrations.Express({ app }),
+    ],
+
+    tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(rateLimiter);
 
 app.use(express.json());
@@ -29,6 +47,20 @@ app.use("/cars", express.static(`${upload.tmpFolder}/carImages`));
 app.use(cors());
 
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
+
+app.use(
+    Sentry.Handlers.errorHandler({
+        shouldHandleError(error) {
+            // Capture all 404 and 500 errors
+            if (error.status === 429 || error.status === 500) {
+                return true;
+            }
+            return false;
+        },
+    })
+);
 
 app.use(
     (err: Error, request: Request, response: Response, next: NextFunction) => {
